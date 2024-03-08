3733b255550e4acaa9acc7087a6bea7c1bae46e5
Nikhil Raj <nikhilraaz045@gmail.com>
2024-03-09 02:31:56 +0530

    Added Initial File

--- app.js
+++ app.js
@@ -1,46 +1,29 @@
-const BASE_URL =
-  "https://v6.exchangerate-api.com/v6/2c7642ab460f0b3a42243b60/latest/";
-
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
 
+// Adding Country Codes in Dropdowns
 for (let select of dropdowns) {
   for (currCode in countryList) {
-    let newOption = document.createElement("option");
-    newOption.innerText = currCode;
-    newOption.value = currCode;
-    if (select.name === "from" && currCode === "USD") {
-      newOption.selected = "selected";
-    } else if (select.name === "to" && currCode === "INR") {
-      newOption.selected = "selected";
+    let option = document.createElement("option");
+    option.innerHTML = currCode;
+    option.value = currCode;
+    select.append(option);
+    if (currCode == "USD" && select.name == "from") {
+      option.selected = "selected";
+    } else if (currCode == "INR" && select.name == "to") {
+      option.selected = "selected";
     }
-    select.append(newOption);
   }
 
-  select.addEventListener("change", (evt) => {
+  select.addEventListener("click", (evt) => {
     updateFlag(evt.target);
   });
 }
 
-const updateExchangeRate = async () => {
-  let amount = document.querySelector(".amount input");
-  let amtVal = amount.value;
-  if (amtVal === "" || amtVal < 1) {
-    amtVal = 1;
-    amount.value = "1";
-  }
-  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
-  let response = await fetch(URL);
-  let data = await response.json();
-  let rate = data[toCurr.value.toLowerCase()];
-
-  let finalAmount = amtVal * rate;
-  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
-};
-
+// Updating Flags whenever Country change in Dropdowns
 const updateFlag = (element) => {
   let currCode = element.value;
   let countryCode = countryList[currCode];
@@ -49,11 +32,29 @@ const updateFlag = (element) => {
   img.src = newSrc;
 };
 
+// Converting Amount and Displaying Result
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = 1;
+  }
+
+  const API_KEY = `https://api.frankfurter.app/latest?amount=${amtVal}&from=${fromCurr.value}&to=${toCurr.value}`;
+  let response = await fetch(API_KEY);
+  let data = await response.json();
+  let rate = data.rates[toCurr.value];
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${rate} ${toCurr.value}`;
+};
+
+// Adding Event Listener to Buttons
 btn.addEventListener("click", (evt) => {
   evt.preventDefault();
   updateExchangeRate();
 });
 
+// Whenever Page refreshes Result will show of predefined value
 window.addEventListener("load", () => {
   updateExchangeRate();
 });
